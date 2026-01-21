62a344c271c3a86094b3b9d6c09f347cbe762153
ranselorm <ranselorm@outlook.com>
2026-01-21 22:54:28 +0000

    feat: add database schema for departments and subjects

---
+++ classroom-backend/src/db/schema/app.ts
@@ -0,0 +1,28 @@
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+};
+
+export const departments = pgTable("departments", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar("name", { length: 100 }).notNull(),
+  code: varchar("code", { length: 50 }).notNull().unique(),
+  description: varchar("name", { length: 255 }),
+  ...timestamps,
+});
+
+export const subjects = pgTable("subjects", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  departmentId: integer("department_id")
+    .notNull()
+    .references(() => departments.id, { onDelete: "restrict" }),
+  name: varchar("name", { length: 100 }).notNull(),
+  code: varchar("code", { length: 50 }).notNull().unique(),
+  description: varchar("name", { length: 255 }),
+  ...timestamps,
+});
--- classroom-backend/src/db/schema/index.ts
+++
@@ -1,1 +0,0 @@
-import * from './app.ts'
